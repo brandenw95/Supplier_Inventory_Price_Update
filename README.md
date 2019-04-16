53f41ffe4433e290dd577f43f4e94e66e2feb150
Branden <36615598+brandenw95@users.noreply.github.com>
2019-04-15 23:24:51 -0700

    Added stocking and policy set

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # prostreet
-All Prostreet Project code
+Code to help with the sorting of product data from supplier and agrigate it into the backend of prostreet.ca
--- sort.py
+++ sort.py
@@ -4,8 +4,9 @@ import re
 import csv
 import pandas as pd
 
-#GLOBAL_VARIABLES
+#Premier FTP Support email: datateam@premierwd.com
 
+#GLOBAL_VARIABLES
 PROSTREET_IN = "prostreet.csv"
 PREMIERWD_IN = "premier.csv" 
 PROSTREET_OUT = "UPLOAD.csv" 
@@ -18,11 +19,18 @@ PROSTREET_OUT = "UPLOAD.csv"
 def proccess_line(export_line, FILEOUT, header_export):
         #Proccess each line of the file exported from the website
 
-        # Variable declaration
+        #Variable declaration for Prostreet Export
         export_sku_index = header_export.index('Variant SKU')
         export_price_index = header_export.index('Variant Price')
+        export_policy_index = header_export.index('Variant Inventory Policy')
+        export_qty_index = header_export.index('Variant Inventory Qty')
+        export_weight_index = header_export.index('Variant Grams')
+        export_policy = export_line[export_policy_index]
         export_sku = export_line[export_sku_index]
         export_price = export_line[export_price_index]
+        export_qty = export_line[export_qty_index]
+        export_weight = export_line[export_weight_index]
+
         
         #open the supplier file (Wont loop every line elsewise)
         read_in_one = open(PREMIERWD_IN, 'r', encoding="utf8")
@@ -32,23 +40,42 @@ def proccess_line(export_line, FILEOUT, header_export):
         #define the sku and the price of the suppliers info
         supplier_price_index = header_supplier.index('MAP')
         supplier_sku_index = header_supplier.index('VendorPartNumber')
+        supplier_stocking_index = header_supplier.index('Status')
+
 
         #Create a copy of the current row of export
         row = []
         row = export_line
 
         #Check each line in PROSTREET_IN and compare with every line in the PREMIERWD file 
+        # - Updates price
+        # - Sets Policy to deny so items with 0 QTY show as "Sold out" / "Special order"
+        # - Sets all Products not stocked by premier to QTY 0 
         for supplier_line in supplier_file:
+                
                 
                 if (export_sku == supplier_line[supplier_sku_index]):
 
+                        #Set Price
                         row[export_price_index] = float(supplier_line[supplier_price_index].strip('$'))
+                        
+                        #Set Policy
+                        row[export_policy_index] = 'deny'
+                        
+                        #Set Stocking Check
+                        if(supplier_line[supplier_stocking_index] == 'Non Stocking'):
+                                row[export_qty_index] = 0
+                        
+                        #Write row to file
                         FILEOUT.writerow(row)
+
                 else:
                         pass
+                
         
         read_in_one.close()
         
+
 def main():
 
         # Open file: website export, supplier export and create writable file
